640c96c9b5f3bbac6fa4c80308401b08849646b7
Mahu Muhammed-Ali I <amahu90@gmail.com>
2024-12-16 11:23:41 +0300

    мир 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,67 @@
-//==============================================================================
-///
-/// @file
-/// @brief Программа для робота Карела и стартовая функция
-///
-//==============================================================================
-
-
 #include "karel.h"
-
-
-//==============================================================================
-/// Программа для робота Карела
 ///
 void KarelProgram() {
 /// @todo Добавить программу для Карела
+int p = 0;
 
+TurnLeft();
+
+while(FrontIsClear() == true){
+    TurnLeft();
+    TurnLeft();
+    TurnLeft();
+    while(FrontIsClear() == true){
+        while(BeepersPresent() == true){
+            PickBeeper();
+            p += 2;
+        }
+        for(int i = 0; i < p; i++){
+            PutBeeper();
+        }
+        p = 0;
+        Move();
+    }
+    while(BeepersPresent() == true){
+        PickBeeper();
+        p += 2;
+    }
+    for(int i = 0; i < p; i++){
+        PutBeeper();
+    }
+    p = 0;
+    TurnLeft();
+    if (FrontIsClear() == true){
+        Move();
+        TurnLeft();
+       while(FrontIsClear() == true){
+            Move();
+        }
+    }
+    TurnLeft();
+    TurnLeft();
+    TurnLeft();
+}
+TurnLeft();
+TurnLeft();
+TurnLeft();
+while(FrontIsClear() == true){
+        while(BeepersPresent() == true){
+            PickBeeper();
+            p += 2;
+        }
+        for(int i = 0; i < p; i++){
+            PutBeeper();
+        }
+        p = 0;
+        Move();
+    }
+    while(BeepersPresent() == true){
+        PickBeeper();
+        p += 2;
+    }
+    for(int i = 0; i < p; i++){
+        PutBeeper();
+    }
 }
 
 
@@ -22,8 +69,7 @@ void KarelProgram() {
 /// Стартовая функция
 ///
 int main() {
-    LoadWorld("worlds/.w");
+    LoadWorld("worlds/Double_beepers_01.w");
     KarelProgram();
     Finish();
-    return 0;
-}
+    return 0;}
